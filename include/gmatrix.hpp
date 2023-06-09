a2240fa10b10d14de8d18cffee85fc30fb50da86
Ax-Time <giacomo1307@gmail.com>
2023-06-10 00:34:50 +0200

    Implemented reshape method

--- include/gmatrix.hpp
+++ include/gmatrix.hpp
@@ -18,12 +18,21 @@ private:
     std::vector<size_t> indices;
 public:
     indexer(std::vector<size_t>* shape, T** data, size_t idx) {
+        if (idx >= (*shape)[0]) {
+            throw std::out_of_range("Index out of boundaries.");
+        }
         this->shape = shape;
         this->data = data;
         this->indices.push_back(idx);
     }
 
     indexer operator[] (size_t idx) {
+        if (indices.size() == shape->size()) {
+            throw std::runtime_error("Trying to index a scalar.");
+        }
+        if (idx >= (*shape)[indices.size()]) {
+            throw std::out_of_range("Index out of boundaries.");
+        }
         this->indices.push_back(idx);
         return *this;
     }
@@ -63,7 +72,7 @@ public:
 template <typename T>
 class gmatrix {
 private:
-    std::vector<size_t> shape;
+    std::vector<size_t> _shape;
     T* data;
     size_t size;
 public:
@@ -72,15 +81,37 @@ public:
         size_t size = 1;
         for (auto s : shape) {
             if (s == 0) throw std::invalid_argument("Cannot create zero-shaped matrix.");
-            this->shape.push_back(s);
+            this->_shape.push_back(s);
             size *= s;
         }
         data = new T[size];
         this->size = size;
     }
 
+    ~gmatrix() {
+        delete[] data;
+    }
+
+    std::vector<size_t> shape() {
+        return this->_shape;
+    }
+    
+    void reshape(std::initializer_list<size_t> shape) {
+        if (shape.size() == 0) throw std::invalid_argument("Cannot reshape matrix to empty shape");
+        size_t after_size = 1;
+        for (auto sh : shape) {
+            if (sh == 0) throw std::invalid_argument("Cannot reshape matrix to zero shape.");
+            after_size *= sh;
+        }
+        if (after_size != this->size) throw std::invalid_argument("Cannot reshape matrix to different size.");
+        this->_shape.clear();
+        for (auto sh : shape) {
+            this->_shape.push_back(sh);
+        }
+    }
+
     indexer<T> operator[] (size_t idx) {
-        return indexer<T>(&this->shape, &this->data, idx);
+        return indexer<T>(&this->_shape, &this->data, idx);
     }
 
     friend class indexer<T>;
--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,40 @@
 #include "gmatrix.hpp"
 
 int main() {
-    
+    ax::gmatrix<float> m({5, 2});
+
+    std::cout << "Matrix shape: ";
+    for (size_t i = 0; i < m.shape().size(); i++) {
+        std::cout << m.shape()[i] << " ";
+    }
+    std::cout << std::endl;
+
+    for (size_t i = 0; i < m.shape()[0]; i++) {
+        for (size_t j = 0; j < m.shape()[1]; j++) {
+            m[i][j] = i * j;
+        }
+    }
+
+    for (size_t i = 0; i < m.shape()[0]; i++) {
+        for (size_t j = 0; j < m.shape()[1]; j++) {
+            std::cout << m[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    m.reshape({10});
+
+    std::cout << "Matrix shape: ";
+    for (size_t i = 0; i < m.shape().size(); i++) {
+        std::cout << m.shape()[i] << " ";
+    }
+    std::cout << std::endl;
+
+    for (size_t i = 0; i < m.shape()[0]; i++) {
+        std::cout << m[i] << " ";
+    }
+    std::cout << std::endl;
+
+
+    return 0;
 }
