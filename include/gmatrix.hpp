7084ba1853deefa7d74a6afd5e7bbbc35ed33e5f
Ax-Time <giacomo1307@gmail.com>
2023-06-10 16:20:39 +0200

    Implemented tensor product

--- include/gmatrix.hpp
+++ include/gmatrix.hpp
@@ -1,5 +1,5 @@
-#ifndef AX_MATRIX_HPP
-#define AX_MATRIX_HPP
+#ifndef AX_GMATRIX_HPP
+#define AX_GMATRIX_HPP
 
 #include <cstddef>
 #include <initializer_list>
@@ -17,6 +17,10 @@ private:
     T** data;
     std::vector<size_t> indices;
 public:
+    indexer(std::vector<size_t>* shape, T** data) {
+        this->shape = shape;
+        this->data = data;
+    }
     indexer(std::vector<size_t>* shape, T** data, size_t idx) {
         if (idx >= (*shape)[0]) {
             throw std::out_of_range("Index out of boundaries.");
@@ -35,6 +39,14 @@ public:
         }
         this->indices.push_back(idx);
         return *this;
+    }
+
+    void clear() {
+        this->indices.clear();
+    }
+
+    void erase_last() {
+        this->indices.pop_back();
     }
 
     operator T&() const {
@@ -73,9 +85,9 @@ template <typename T>
 class gmatrix {
 private:
     std::vector<size_t> _shape;
-    T* data;
     size_t size;
 public:
+    T* data;
     gmatrix(std::initializer_list<size_t> shape) 
         : gmatrix(std::vector<size_t>(shape)) { }
 
@@ -91,28 +103,41 @@ public:
         this->size = size;
     }
 
+    gmatrix(size_t m, size_t n) 
+        : gmatrix(std::vector<size_t>({m, n})) {}
+
     ~gmatrix() {
         delete[] data;
     }
 
-    std::vector<size_t> shape() {
+    std::vector<size_t> shape() const {
         return this->_shape;
     }
-    
-    void reshape(std::initializer_list<size_t> shape) {
-        if (shape.size() == 0) throw std::invalid_argument("Cannot reshape matrix to empty shape");
-        size_t after_size = 1;
-        for (auto sh : shape) {
-            if (sh == 0) throw std::invalid_argument("Cannot reshape matrix to zero shape.");
-            after_size *= sh;
-        }
-        if (after_size != this->size) throw std::invalid_argument("Cannot reshape matrix to different size.");
-        this->_shape.clear();
-        for (auto sh : shape) {
-            this->_shape.push_back(sh);
-        }
-    }
 
+    size_t get_size() const {
+        return this->size;
+    }
+    
+    /**
+     * Matrix should be immutable, so we don't allow reshaping in place for now.
+    */
+    // void reshape(std::initializer_list<size_t> shape) {
+    //     if (shape.size() == 0) throw std::invalid_argument("Cannot reshape matrix to empty shape");
+    //     size_t after_size = 1;
+    //     for (auto sh : shape) {
+    //         if (sh == 0) throw std::invalid_argument("Cannot reshape matrix to zero shape.");
+    //         after_size *= sh;
+    //     }
+    //     if (after_size != this->size) throw std::invalid_argument("Cannot reshape matrix to different size.");
+    //     this->_shape.clear();
+    //     for (auto sh : shape) {
+    //         this->_shape.push_back(sh);
+    //     }
+    // }
+
+    /**
+     * Sums two tensors of the same shape
+    */
     gmatrix operator+ (gmatrix<T>& other) {
         if (!same_shape(*this, other)) throw std::invalid_argument("Cannot add matrices with different shapes.");
         gmatrix<T> result(this->_shape);
@@ -122,8 +147,79 @@ public:
         return result;
     }
 
+    /**
+     * Computes the tensor product of two tensors:
+     * A shape = (a1, a2, ..., an)
+     * B shape = (b1, b2, ..., bm)
+     * It can be computed only if an == b1
+     * A x B shape = (a1, a2, ..., a(n-1), b2, ..., bm)
+    */
+    gmatrix operator* (gmatrix<T>& other) {
+        if (!can_perform_product(*this, other)) throw std::invalid_argument("Cannot multiply uncompatible matrices.");
+        std::vector<size_t> res_shape;
+        res_shape.insert(res_shape.end(), _shape.begin(), _shape.end() - 1);
+        res_shape.insert(res_shape.end(), other._shape.begin() + 1, other._shape.end());
+        gmatrix<T> result(res_shape);
+        memset(result.data, 0, result.size);
+        std::vector<size_t> multipliers;
+        multipliers.push_back(1);
+        for (int i = result._shape.size() - 2; i >= 0; i--) {
+            multipliers.insert(multipliers.begin(), *multipliers.begin() * result._shape[i + 1]);
+        }
+
+        // Start operation
+        for (size_t idx = 0; idx < result.size; idx++) {
+            std::vector<size_t> indices;
+
+            size_t temp = idx;
+            for (size_t mul : multipliers) {
+                indices.push_back(temp / mul);
+                temp = temp % mul;
+            }
+            
+            indexer<T> result_indexer(&result._shape, &result.data);
+            indexer<T> left_indexer(&this->_shape, &this->data);
+
+            
+            // Index the result matrix
+            for (auto i : indices) {
+                result_indexer = result_indexer[i];
+            }
+
+            // Index the left matrix
+            for (size_t i = 0; i < this->_shape.size() - 1; i++) {
+                left_indexer = left_indexer[indices[i]];
+            }
+
+            indexer<T> right_indexer(&other._shape, &other.data);
+            for (size_t k = 0; k < other._shape[0]; k++) {
+                auto r_indexer = right_indexer[k];
+
+                // Index the right matrix
+                for (size_t i = result._shape.size() - other._shape.size() + 1; i < indices.size(); i++) {
+                    r_indexer = r_indexer[indices[i]];
+                }
+
+                T& value = result_indexer;
+                T& left = left_indexer[k];
+                T& right = r_indexer;
+                value += left * right;
+
+                // Clean up
+                right_indexer.clear();
+                left_indexer.erase_last();
+            }
+        }
+        return result;
+    }
+
+
     // Static methods
-    bool same_shape(gmatrix<T>& m1, gmatrix<T>& m2) {
+    static bool can_perform_product(gmatrix<T>& m1, gmatrix<T>& m2) {
+        return m1._shape[m1._shape.size() - 1] == m2._shape[0];
+    }
+
+    static bool same_shape(gmatrix<T>& m1, gmatrix<T>& m2) {
         if (m1.shape().size() != m2.shape().size()) return false;
         for (size_t i = 0; i < m1.shape().size(); i++) {
             if (m1.shape()[i] != m2.shape()[i]) return false;
@@ -132,7 +228,6 @@ public:
     }
 
     // Indexer
-
     indexer<T> operator[] (size_t idx) {
         return indexer<T>(&this->_shape, &this->data, idx);
     }
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,33 @@
 #include "gmatrix.hpp"
 
 int main() {
-    ax::gmatrix<int> m{3, 6, 8, 7, 6};
+    ax::gmatrix<float> m1({6, 3, 4});
+    ax::gmatrix<float> m2({4, 6, 3});
+
+    // Initialize the matrices with subsequent numbers
+    size_t counter = 0;
+    for (size_t i = 0; i < m1.shape()[0]; i++) {
+        for (size_t j = 0; j < m1.shape()[1]; j++) {
+            for (size_t k = 0; k < m1.shape()[2]; k++) {
+                m1[i][j][k] = counter++;
+            }
+        }
+    }
+
+    counter = 0;
+    for (size_t i = 0; i < m2.shape()[0]; i++) {
+        for (size_t j = 0; j < m2.shape()[1]; j++) {
+            for (size_t k = 0; k < m2.shape()[2]; k++) {
+                m2[i][j][k] = counter++;
+            }
+        }
+    }
+
+    // Product
+    ax::gmatrix<float> m3 = m1 * m2;
+
+
+    std::cout << m3[5][2][2][1] << std::endl;
 
     return 0;
 }
